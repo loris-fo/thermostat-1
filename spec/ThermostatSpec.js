7a0f3ce2781a73f8f040cb20485d98a0dc080780
Alex Peattie <alexpeattie@gmail.com>
2014-06-26 17:17:46 +0100

    Completed thermostat

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,100 @@
+describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it("is intially at 20 degrees", function(){
+    expect(thermostat.temperature).toEqual(20)
+  })
+
+  it("has PSM on by default", function(){
+    expect(thermostat.powerSavingMode).toBe(true);
+  })
+
+  describe('maximum temperature', function(){
+    it('is 25 degrees with PSM on', function(){
+      expect(thermostat.maximumTemperature()).toEqual(25);
+    })
+
+    it('is 32 degrees with PSM off', function(){
+      thermostat.powerSavingMode = false;
+      expect(thermostat.maximumTemperature()).toEqual(32);
+    })
+  })
+
+  describe('minimum temperature', function(){
+    it('is 10 degrees', function(){
+      expect(thermostat.minimumTemperature()).toEqual(10);
+    })
+  });
+
+  describe('warmer', function(){
+    describe('PSM on', function(){
+      it('increases the temperature if < 25', function(){
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(21)
+      })
+
+      it('does nothing the temperature if >= 25', function(){
+        thermostat.temperature = 25
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(25)
+      })
+    })
+
+    describe('PSM off', function(){
+      it('increases the temperature if < 32', function(){
+        thermostat.powerSavingMode = false;
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(21)
+      })
+
+      it('does nothing the temperature if >= 32', function(){
+        thermostat.powerSavingMode = false;
+        thermostat.temperature = 32
+        thermostat.warmer();
+        expect(thermostat.temperature).toEqual(32)
+      })
+    })
+  })
+
+  describe('cooler', function(){
+    it('decreases the temperature if > 10', function(){
+      thermostat.cooler();
+      expect(thermostat.temperature).toEqual(19)
+    })
+
+    it('does nothing the temperature if <= 10', function(){
+      thermostat.temperature = 10
+      thermostat.cooler();
+      expect(thermostat.temperature).toEqual(10)
+    })
+  })
+
+  describe('reset', function(){
+    it('resets the temperature to 20', function(){
+      thermostat.temperature = 25
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20)
+    });
+  })
+
+  describe('energy usage', function(){
+    it('is high if >= 25', function(){
+      thermostat.temperature = 26
+      expect(thermostat.energyUsage()).toEqual('high')
+    });
+
+    it('is medium if >= 18 and < 25', function(){
+      thermostat.temperature = 20
+      expect(thermostat.energyUsage()).toEqual('medium')
+    });
+
+    it('is low if < 18', function(){
+      thermostat.temperature = 16
+      expect(thermostat.energyUsage()).toEqual('low')
+    });
+  })
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,37 @@
+function Thermostat() {
+  this.temperature = 20;
+  this.powerSavingMode = true;
+}
+
+Thermostat.prototype.maximumTemperature = function() {
+  if(this.powerSavingMode) {
+    return 25;
+  }
+  return 32;
+};
+
+Thermostat.prototype.minimumTemperature = function() {
+  return 10;
+}
+
+Thermostat.prototype.warmer = function() {
+  if(this.temperature < this.maximumTemperature()) {
+    this.temperature += 1;
+  }
+}
+
+Thermostat.prototype.cooler = function() {
+  if(this.temperature > this.minimumTemperature()) {
+    this.temperature -= 1;
+  }
+}
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
+}
+
+Thermostat.prototype.energyUsage = function() {
+  if(this.temperature >= 25) return 'high';
+  if(this.temperature >= 18) return 'medium';
+  return 'low';
+}
