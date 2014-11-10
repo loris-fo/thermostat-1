928d54b5e3aa4eba4b083db05d565bd7032a3b11
Nadav <nadav.matalon@gmail.com>
2014-11-10 04:30:48 +0000

    refactored JSON call to get weather temperature

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -7,13 +7,21 @@ function Weather() {
 
 function getWeatherTemperature() {
     var currentTemperature;
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric", function(data) {
-        currentTemperature = data.main.temp.toFixed();
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric',
+        async: false,  
+        success: function(data) {
+            currentTemperature = data.main.temp.toFixed();
+        },
+        error: function() { 
+            currentTemperature = '-';
+        } 
     });
     return currentTemperature;
 }
 
 Weather.prototype.getBackgroundColor = function(temp) {
+    if(this.temperature === '-') return 'rgb(220, 220, 220)';
     if(this.temperature >= 25) return 'rgb(200, 45, 45)';
     if(this.temperature >= 18) return 'orange';
     return 'green';
